12e1425737825e1ea0672875370bac1a38dbf057
ysliu <ysliu@fengyuntec.com>
2021-11-22 15:26:51 +0800

    feat:第一次

---
+++ 01/src/index.js
@@ -0,0 +1,12 @@
+  /** 
+   * 入口
+   *  开发话环境打包 webpack ./src/index.js -o ./build/index.js --mode=development  
+   * 
+  */
+
+  function add(x, y){
+
+    return x + y
+
+  }
+  console.log(add(1,1))
---
+++ README.md
@@ -0,0 +1,9 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: 刘永帅
+ * @Date: 2021-11-22 15:22:12
+ * @LastEditors: 刘永帅
+ * @LastEditTime: 2021-11-22 15:23:22
+-->
+# 联系
