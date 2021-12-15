611349b24c38fcf5ea84fa52167b9d2e4625c96f
ysliu <ysliu@fengyuntec.com>
2021-12-15 10:42:28 +0800

    feat：

---
+++ dist/postcss.config.dev.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: 刘永帅
+ * @Date: 2021-12-15 10:25:43
+ * @LastEditors: 刘永帅
+ * @LastEditTime: 2021-12-15 10:25:44
+ */
+module.exports = {
+  plugins: [[//postcss-preset-env插件读取package.json文件中的browserslist配置
+  require('postcss-preset-env')()]]
+};
